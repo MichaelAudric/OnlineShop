2a892e1d851e7168a22005c04f36651c80b5f399
MichaelAudric <127097147+MichaelAudric@users.noreply.github.com>
2024-11-21 00:43:55 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Michael290604.",
+  password: ".", //input password
   database: "online_shop",
 });
 
